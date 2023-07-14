988c2f75b2553a97b2304ff34ff0fb98d26761ce
thawanrat43 <129850059+thawanrat43@users.noreply.github.com>
2023-07-14 10:20:21 +0700

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './page/Home'
 import Login from './page/Login'
 import Register from './page/Register';
-import Profile from './page/profile';
-import Code from './page/Code';
-import Pagestatus from './page/Pagestatus';
-import History from './page/History';
-import Pay from './page/Pay';
-import Qrcode from './page/Qrcode';
-import Finish from './page/Finish';
+// import Profile from './page/profile';
+// import Code from './page/Code';
+// import Pagestatus from './page/Pagestatus';
+// import History from './page/History';
+// import Pay from './page/Pay';
+// import Qrcode from './page/Qrcode';
+// import Finish from './page/Finish';
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
